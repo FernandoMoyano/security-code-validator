777c98c051348d03e158bb02d1b3d2db1fe5a951
FernandoMoyano <fernandomoyano21@gmail.com>
2024-02-13 14:13:57 -0300

    add-status-reactHooks

--- src/ClassState.jsx
+++ src/ClassState.jsx
@@ -1,10 +1,11 @@
+/* eslint-disable react/prop-types */
 import React from "react";
 
 class ClassState extends React.Component {
   render() {
     return (
       <div>
-        <h2>Eliminar ClassState</h2>
+        <h2>Eliminar {this.props.name}</h2>
 
         <p>Por favor, escribe el código de seguridad.</p>
 
--- src/Usestate.jsx
+++ src/Usestate.jsx
@@ -1,12 +1,17 @@
-function UseState() {
+import React from "react";
+
+/* eslint-disable react/prop-types */
+function UseState(props) {
+  const [error, setError] = React.useState(false);
   return (
     <div>
-      <h2>Eliminar UseState</h2>
+      <h2>Eliminar {props.name}</h2>
 
       <p>Por favor, escribe el código de seguridad.</p>
+      {error && <p>Error: El codigo es incorrecto</p>}
 
       <input placeholder='Código de seguridad' />
-      <button>Comprobar</button>
+      <button onClick={() => setError(!error)}>Comprobar</button>
     </div>
   );
 }
